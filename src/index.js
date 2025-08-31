9158a9f2f932c82253bf65f689fb5d65910276f2
FrancoMostafa <89807094+FrancoMostafa@users.noreply.github.com>
2025-08-31 18:15:49 -0300

    🏗️ Add public folder and base React setup

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Si quieres medir el rendimiento de tu app, pasa una función
+// para registrar los resultados (por ejemplo: reportWebVitals(console.log))
+// o envía a un servicio de análisis. Aprende más: https://bit.ly/CRA-vitals
+reportWebVitals();
